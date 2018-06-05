701e404601bf454276dddccbde8a5eea68ba7ec8
Clarissa Schwarz-Linnet <clarissa.schwarz@gmx.net>
2018-06-05 11:01:56 +0200

    all files

---
+++ forms2/main.js
@@ -0,0 +1,56 @@
+const database = firebase.database();
+
+const headerEl = document.querySelector("#form_header");
+
+const descriptionEl = document.querySelector("#form_description");
+
+const form = document.querySelector("form");
+
+const template = document.querySelector("#noteTemplate").content;
+
+const app = document.querySelector("#app")
+
+//ad new notes
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    database.ref("notes/").push({
+        header: headerEl.value,
+        description: descriptionEl.value
+    });
+
+    // clear form
+    headerEl.value = "";
+    descriptionEl.value = "";
+});
+
+
+
+//listen for new data
+
+database.ref("notes/").on("child_added", (snapshot) => {
+    console.log(snapshot);
+
+    const key = snapshot.key;
+    const data = snapshot.val();
+    console.log(key);
+    console.log(data);
+
+    const clone = template.cloneNode(true);
+
+    clone.querySelector("article").dataset.key=key;
+
+    clone.querySelector("h1").textContent = data.header;
+    clone.querySelector("div.description").textContent = data.description;
+    clone.querySelector("button.delete").addEventListener("click", e=>{
+            database.ref("notes/"+key).remove();
+    });
+
+    app.appendChild(clone);
+});
+
+//listen for removal of data
+database.ref("notes/").on("child_removed", snapshot => {
+    const key = snapshot.key;
+    let el = document.querySelector(`article[data-key=${key}]`);
+    el.remove();
+});
